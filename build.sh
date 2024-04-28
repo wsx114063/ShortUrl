a09850a1c5a0571b4b5aa5dd201828595e9b1c21
kuro.chen <kuro.chen@hgiga.com>
2024-04-28 22:04:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ GO_DOWNLOAD_URL="https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
 
 wget -q "${GO_DOWNLOAD_URL}" -O /tmp/go.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+
 export GOROOT="/usr/local/go"
-export GOPATH="$HOME/go"
-export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+export PATH="${GOROOT}/bin:${PATH}"
+
+go version
 
 go build -o gobuild
