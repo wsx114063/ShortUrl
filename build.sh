6467f3ba9cbf92a2d7c676d4dc9821b72ebadbb7
kuro.chen <kuro.chen@hgiga.com>
2024-04-28 21:34:49 +0800

    Update build.sh
    in build.sh install golang

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-apt-get update && apt-get install -y golang
+#!/bin/bash
+GO_VERSION="1.22.2"
+GO_DOWNLOAD_URL="https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+wget -q "${GO_DOWNLOAD_URL}" -O /tmp/go.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+export GOROOT="/usr/local/go"
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+
 go build -o gobuild
